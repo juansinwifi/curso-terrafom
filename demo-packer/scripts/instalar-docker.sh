23607a6f0de904b03cd56a864e9d74a1de76147b
Juan Sebastian Otero <ing.sebastianotero@gmail.com>
2019-11-24 21:10:38 -0500

    Update to work with ubuntu

--- demo-packer/scripts/instalar-docker.sh
+++ demo-packer/scripts/instalar-docker.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 # Instalar Docker
-sudo yum install docker-ce docker-ce-cli containerd.io
-sudo systemctl start docker
+sudo apt update
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install -y docker-ce
+sudo systemctl status docker
 
-#Agregar el usuario centos al grupo docker
-sudo groupadd docker
-sudo usermod -aG docker centos
+#Agregar el usuario ubuntu al grupo docker
+sudo usermod -aG docker ${USER}
